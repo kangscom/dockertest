093267e6a435778bb9d5fc57dca8d348de324c2b
kangscom <72960086+kangscom@users.noreply.github.com>
2023-03-10 19:03:07 +0900

    Update MergeRequestNotifer.java

--- MergeRequestNotifer.java
+++ MergeRequestNotifer.java
@@ -17,7 +17,42 @@ public class MergeRequestNotifer {
     //STEP-1 : WebHook이 보내준 정보를 이용해서 GitLab 에서 진행중인 MR목록을 가져온다.
     String mrList = getMergeReqeustList(MERGE_REQUEST_URL);
     
-  
+    //STEP-2 : MR 목록에서 WebHook 정보와 일치하는 리뷰건을 찾고 Reviewer ID 를 가져온다.
+    String reviewerId = getReviewerId(mrList);
+    
+    //STEP-3 : reviewerId에 맵핑된 리뷰어 그룹에게 mail을 보내어 코드리뷰를 수행하도록 Noti한다.
+    sendMail(reviewerId);
+  }
+   
+  private static void sendMail(String reviewerId) {
+    String reviewers = maillingList.get(reviewerId);
+    
+    for( String mailId : reviwers.split(",")) {
+      System.out.println("reviewer_group :: " + reviwerId + ", sending mailg to= " _ maildId.trim()); 
+    }
+  }
+   
+   
+  private static String getReviewerId(String mrList) {
+    String reviewerId = "";
+    List<Map<String, String>> list = JsonPath.read(mrList, "$");
+    
+    for( Map map : list) {
+      List<Map> reviewerList = (List)map.get("reviewers");
+      Map<String, String> reviewerMap = reviewerList.get(0);
+      reviewerId = reviewerMap.get("username");
+      break;
+    }
+    return reviewerId;
   }
 
+   
+  private static String getMergeRequestList(String merge_request_url) throws Exception {
+    return Jsoup.connect(merge_request_url)
+            .header("Content-Type", "application/json")
+            .header("PRIVATE_TOKEN", GITLAB_API_PRIVATE_KEY)
+            .ignoreContentType(true)
+            .execute()
+            .body();
+  }
 }
